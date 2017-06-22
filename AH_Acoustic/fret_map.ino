07f899bcd3f0c4fb4f53c7ee125e70fa4fc3d07d
MagicalGentleman <superquinn@naver.com>
2017-06-22 13:54:47 +0100

    made input scan LUT based because past me was dumb

--- AH_Acoustic/fret_map.ino
+++ AH_Acoustic/fret_map.ino
@@ -1,101 +1,42 @@
 byte frets(boolean g, boolean r, boolean y, boolean b, boolean o, byte oct) {
-  byte octave=oct;
-  byte octave2=(octave+1);
-  byte octave3=(octave2+1);
-  // ----- // C
-  if((!g)&&(!r)&&(!y)&&(!b)&&(!o)) return pitch(0, octave);
   
-  // G---- // CS
-  else if(g&&(!r)&&(!y)&&(!b)&&(!o)) return pitch(1, octave);
+  byte noteLUT[32] = {
+    0x00, // ----- // C 
+    0x05, // ----O // F 
+    0x04, // ---B- // E 
+    0x09, // ---BO // A 
+    0x03, // --Y-- // DS
+    0x12, // --Y-O // D 
+    0x08, // --YB- // GS
+    0x13, // --YBO // DS
+    0x02, // -R--- // D 
+    0x18, // -R--O // GS
+    0x10, // -R-B- // C 
+    0x1A, // -R-BO // AS
+    0x07, // -RY-- // G 
+    0x19, // -RY-O // A 
+    0x11, // -RYB- // CS
+    0x1B, // -RYBO // B 
+    0x01, // G---- // CS
+    0x20, // G---O // C 
+    0x14, // G--B- // E 
+    0x23, // G--BO // DS
+    0x0A, // G-Y-- // AS
+    0x22, // G-Y-O // D 
+    0x16, // G-YB- // FS
+    0x25, // G-YBO // F 
+    0x06, // GR--- // FS
+    0x21, // GR--O // CS
+    0x15, // GR-B- // F 
+    0x26, // GR-BO // FS
+    0x0B, // GRY-- // B
+    0x24, // GRY-O // E
+    0x17, // GRYB- // G
+    0x27  // GRYBO // G
+  };
   
-  // -R--- // D
-  else if((!g)&&r&&(!y)&&(!b)&&(!o)) return pitch(2, octave);
+  int index = ((int(g)&1)<<4) | ((int(r)&1)<<3) | ((int(y)&1)<<2) | ((int(b)&1)<<1) | (int(o)&1);
+  byte note = noteLUT[index];
   
-  // --Y-- // DS
-  else if((!g)&&(!r)&&y&&(!b)&&(!o)) return pitch(3, octave);
-  
-  // ---B- // E
-  else if((!g)&&(!r)&&(!y)&&b&&(!o)) return pitch(4, octave);
-  
-  // ----O // F
-  else if((!g)&&(!r)&&(!y)&&(!b)&&o) return pitch(5, octave);
-  
-  // GR--- // FS
-  else if(g&&r&&(!y)&&(!b)&&(!o)) return pitch(6, octave);
-  
-  // -RY-- // G
-  else if((!g)&&r&&y&&(!b)&&(!o)) return pitch(7, octave);
-  
-  // --YB- // GS
-  else if((!g)&&(!r)&&y&&b&&(!o)) return pitch(8, octave);
-  
-  // ---BO // A
-  else if((!g)&&(!r)&&(!y)&&b&&o) return pitch(9, octave);
-  
-  // G-Y-- // AS
-  else if(g&&(!r)&&y&&(!b)&&(!o)) return pitch(10, octave);
-  
-  // GRY-- // B
-  else if(g&&r&&y&&(!b)&&(!o)) return pitch(11, octave);
-  
-  // -R-B- // C
-  else if((!g)&&r&&(!y)&&b&&(!o)) return pitch(0, octave2);
-  
-  // -RYB- // CS
-  else if((!g)&&r&&y&&b&&(!o)) return pitch(1, octave2);
-  
-  // --Y-O // D
-  else if((!g)&&(!r)&&y&&(!b)&&o) return pitch(2, octave2);
-  
-  // --YBO // DS
-  else if((!g)&&(!r)&&y&&b&&o) return pitch(3, octave2);
-  
-  // G--B- // E
-  else if(g&&(!r)&&(!y)&&b&&(!o)) return pitch(4, octave2);
-  
-  // GR-B- // F
-  else if(g&&r&&(!y)&&b&&(!o)) return pitch(5, octave2);
-  
-  // G-YB- // FS
-  else if(g&&(!r)&&y&&b&&(!o)) return pitch(6, octave2);
-  
-  // GRYB- // G
-  else if(g&&r&&y&&b&&(!o)) return pitch(7, octave2);
-  
-  // -R--O // GS
-  else if((!g)&&r&&(!y)&&(!b)&&o) return pitch(8, octave2);
-  
-  // -RY-O // A
-  else if((!g)&&r&&y&&(!b)&&o) return pitch(9, octave2);
-  
-  // -R-BO // AS
-  else if((!g)&&r&&(!y)&&b&&o) return pitch(10, octave2);
-  
-  // -RYBO // B
-  else if((!g)&&r&&y&&b&&o) return pitch(11, octave2);
-  
-  // G---O // C
-  else if(g&&(!r)&&(!y)&&(!b)&&o) return pitch(0, octave3);
-  
-  // GR--O // CS
-  else if(g&&r&&(!y)&&(!b)&&o) return pitch(1, octave3);
-  
-  // G-Y-O // D
-  else if(g&&(!r)&&y&&(!b)&&o) return pitch(2, octave3);
-  
-  // G--BO // DS
-  else if(g&&(!r)&&(!y)&&b&&o) return pitch(3, octave3);
-  
-  // GRY-O // E
-  else if(g&&r&&y&&(!b)&&o) return pitch(4, octave3);
-  
-  // G-YBO // F
-  else if(g&&(!r)&&y&&b&&o) return pitch(5, octave3);
-  
-  // GR-BO // FS
-  else if(g&&r&&(!y)&&b&&o) return pitch(6, octave3);
-  
-  // GRBYO // G
-  else return pitch(7, octave3);
-  
+  return  pitch(note&0x0F, oct + ((note&0x30)>>4));
 }
